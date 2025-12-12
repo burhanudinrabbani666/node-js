ef19dd583d23dcd83ac40f7971b8fbe20a04f474
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-12 23:33:21 +0700

    feat: redirecting

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // Routing
   // http://localhost:3000/
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter message</title></head>");
@@ -11,6 +13,15 @@ const server = http.createServer((req, res) => {
       `<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`
     );
     res.write("</html>");
+    return res.end();
+  }
+
+  // Http://localhost:3000/message
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "Hello World");
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
 
     return res.end();
   }
---
+++ notes/section-3/09-redirecting-request.md
@@ -0,0 +1,29 @@
+## Redirecting Request
+
+```js
+=> {
+  // Routing
+  // http://localhost:3000/
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      `<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  // Http://localhost:3000/message this is redirecting
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "Hello World");
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+
+    return res.end();
+  }
+
+```
