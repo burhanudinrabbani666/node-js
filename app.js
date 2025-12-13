3ec3556810b5139e04cb4100de7110c8ee456247
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-13 17:08:43 +0700

    feat: Adding middleware

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ const http = require("http");
 // Framework / 3rd Party
 const express = require("express");
 
+// App
 const app = express();
 
-const server = http.createServer(app);
+//Middleware
+app.use((req, res, next) => {
+  console.log("in The middleWare");
 
+  next(); // for Continue to another Middleware ⬇️
+});
+
+app.use((req, res, next) => {
+  console.log("in Another The middleWare");
+});
+
+// Server
+const server = http.createServer(app);
 server.listen(3000); //http://localhost:3000/
