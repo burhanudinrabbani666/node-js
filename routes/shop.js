ebabd9468d64a3b215a9e4cf75113fb7cc7f81f6
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-16 20:45:46 +0700

    feat: converting normal html to pug

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,8 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   // console.log(adminData.product);
   // res.sendFile(path.join(routDir, "views", "shop.html"));
-
-  res.render("shop");
+  const products = adminData.product;
+  res.render("shop", { prods: products, docsTitle: "Shop" });
 });
 
 module.exports = router;
