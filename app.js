f2686d3fd93102dd7c8c15b8506bacbd04693141
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-18 17:54:38 +0700

    feat: deleting other engine

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.set("views", "views");
 app.use(shopRoute);
 app.use("/admin", adminData.routes);
 
-app.use("/", (req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "" });
 });
 
 // Server
