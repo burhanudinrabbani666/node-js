5dba736f14debfec98961277ab1015b83de2e528
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-19 22:19:15 +0700

    feat; adding input field, new parms for Product class

--- controllers/controllers-admin.js
+++ controllers/controllers-admin.js
@@ -11,8 +11,13 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+
   // save in variable
-  const product = new Product(req.body.title);
+  const product = new Product(title, imageUrl, price, description);
 
   // Using Save Method
   product.save();
